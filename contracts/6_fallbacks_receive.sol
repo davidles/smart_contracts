e62db5f346f18e854ab15f5bc313dd1c27bb21f0
Remix Workspace <>
2022-10-21 14:39:18 -0300

    add styles lines

--- contracts/6_fallbacks_receive.sol
+++ contracts/6_fallbacks_receive.sol
@@ -24,11 +24,20 @@ contract fallback_receive{
     event log( string _name, address _sender, uint _amount, bytes _data );
 
     fallback() external payable {
-        emit log ( "fallback", msg.sender, msg.value, msg.data);
+        emit log (
+            "fallback", 
+            msg.sender, 
+            msg.value, 
+            msg.data
+        );
     }
 
     receive() external payable {
-        emit log( 'receive', msg.sender, msg.value, "");
+        emit log(
+            'receive', 
+            msg.sender, 
+            msg.value, 
+            "");
     }
 
 }
