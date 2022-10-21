5eb427dfa3e84ba3cab34402599e1dbe689d33a6
Remix Workspace <>
2022-10-21 14:54:29 -0300

    created function mod

--- contracts/7_maths.sol
+++ contracts/7_maths.sol
@@ -18,4 +18,8 @@ contract maths{
     function expon(uint a, uint b) public pure returns (uint){
         return a**b;
     }
+
+    function mod(uint a, uint b) public pure returns (uint){
+        return a%b;
+    }
 }
