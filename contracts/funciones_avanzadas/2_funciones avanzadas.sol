b5110bbddccad7ae2e688e271a9e18a65609739e
Remix Workspace <>
2022-10-21 16:43:53 -0300

    add public to dinnerPlate

--- contracts/funciones_avanzadas/2_funciones avanzadas.sol
+++ contracts/funciones_avanzadas/2_funciones avanzadas.sol
@@ -11,7 +11,7 @@ contract Food {
     }
 
     // Menu del dia
-    dinnerPlate [] menu;
+    dinnerPlate [] public menu;
 
     //Creacion de un nuevo menu
     function newMenu(string memory _name, string memory _ingredientes) internal {
