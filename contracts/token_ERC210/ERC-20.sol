ff92ddc353ebea1d5eab1ce33c356fb0d779e1a7
Remix Workspace <>
2022-10-24 14:22:16 -0300

    create token with openzeppelin

---
+++ contracts/token_ERC210/ERC-20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyCollectible is ERC721 {
+    constructor() ERC721("MyCollectible", "MCO") {
+    }
+}
