291c5f7e7aafb4d4b073f01e4aa6790681ee48c8
Remix Workspace <>
2022-10-25 15:54:58 -0300

    create contract erc721

--- contracts/token_ERC721/customERC721.sol
+++ contracts/token_ERC721/customERC721.sol
@@ -7,3 +7,20 @@ import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
 Necesitamos un contador ( ya provisto por OZ ) para darle el Id a los NFTs
 */
 import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
+
+contract erc721 is ERC721{
+
+    // Contadores para los IDs de los NFTs
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokensIds;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){}
+
+    // Envio de NFTs
+    function sendNFT(address _account) public {
+        _tokensIds.increment();
+        uint256 newItemId = _tokensIds.current();
+        _safeMint(_account, newItemId);
+    }
+
+}
