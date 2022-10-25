89155bb19755f195ec7a8c6dcb206257fc17a8a9
Remix Workspace <>
2022-10-25 15:48:37 -0300

    imports smart contracts of openzeppelin repo

--- contracts/token_ERC721/customERC721.sol
+++ contracts/token_ERC721/customERC721.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+// Importacion de smart contracts de OppenZeppelin
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+/**
+Necesitamos un contador ( ya provisto por OZ ) para darle el Id a los NFTs
+*/
+import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
