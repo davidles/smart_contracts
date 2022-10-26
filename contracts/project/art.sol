a0e248290108291043b153aa5c8d1a171bc5811f
Remix Workspace <>
2022-10-26 17:12:13 -0300

    add initial statement

--- contracts/project/art.sol
+++ contracts/project/art.sol
@@ -6,6 +6,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
 
+contract ArtToken is ERC721, Ownable{
 
+    // Initial Statement
+
+    // smart contract constructor
+    constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol){}
+}
 
 
