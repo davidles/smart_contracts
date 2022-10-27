f90564f1b66186f1f442a698fe5f0dae0fa568c7
Remix Workspace <>
2022-10-27 15:55:01 -0300

    Level up NFT tokens

--- contracts/project/art.sol
+++ contracts/project/art.sol
@@ -110,6 +110,13 @@ contract ArtToken is ERC721, Ownable{
         _owner.transfer(address(this).balance);
     }
 
+    // Level up NFT tokens
+    function levelUp(uint256 _artId) public{
+        require(ownerOf(_artId) == msg.sender);
+        Art storage art = art_works[_artId];
+        art.level++;
+    }
+
 }
 
 
